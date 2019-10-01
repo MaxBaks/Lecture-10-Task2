306077df647b85b1ef09174587acb97df207204c
Мишутушкин Максим Александрович <ma.mishutushkin@severstal.com>
2019-10-01 14:44:33 +0300

    01.10

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 import GameSavingLoader from './gameSavingLoader';
 
-const loadSaveData = async (incomeJSON) => {
+const loadSaveData = (incomeJSON) => (async function () {
   try {
     return await GameSavingLoader.load(incomeJSON);
   } catch (error) {
     return new Error('Ошибка загрузки данных!');
   }
-};
+})();
 
 export default loadSaveData;
